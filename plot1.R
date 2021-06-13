83c7ed31979dd13481012e4ab04b7fb7ee47d894
surgeonchen <77779878+surgeonchen@users.noreply.github.com>
2021-06-13 16:19:42 +0200

    Delete plot1.R

--- plot1.R
+++
@@ -1,5 +0,0 @@
-source("read_data.R")
-  
-png(file = "plot1.png", 480, 480)
-hist(final$Global_active_power, main="Global Active Power", xlab = "Global Active Power (kilowatts)", col="red")
-dev.off()
